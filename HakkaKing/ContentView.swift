e272f2ba3afe8c99940e5e988a0c9cb5e9d9b9b5
Amanda <jesslynmulyawan@gmail.com>
2025-06-12 15:17:13 +0700

    Initial Commit

---
+++ HakkaKing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HakkaKing
+//
+//  Created by Amanda on 12/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HakkaKing/HakkaKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HakkaKingApp.swift
+//  HakkaKing
+//
+//  Created by Amanda on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct HakkaKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
